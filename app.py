2c5cb10151ce2bc3ef6274ca44edf0e01d1cd4a4
Leoalwin <alwinleo323@gmail.com>
2026-02-09 16:00:03 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, request,redirect,url_for
 import psycopg2
+import os
 
 app = Flask(__name__)
-conn = psycopg2.connect(
-    database="quizdb",
-    user="postgres",
-    password="leo@123",
-    host="localhost",
-    port="5432"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+conn = psycopg2.connect(DATABASE_URL)
 
 
 
